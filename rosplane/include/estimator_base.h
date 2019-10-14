1bb8a5829719c2128631a8b373e7e3eee93b1ca8
natetoombs <37413999+natetoombs@users.noreply.github.com>
2019-10-14 09:35:42 -0600

    2019 Archive

--- rosplane/include/estimator_base.h
+++ rosplane/include/estimator_base.h
@@ -10,18 +10,21 @@
 #define ESTIMATOR_BASE_H
 
 #include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
 #include <rosplane_msgs/State.h>
-#include <sensor_msgs/NavSatFix.h>
+// #include <rosflight_msgs/GPS.h>
+#include <inertial_sense/GPS.h>
 #include <sensor_msgs/Imu.h>
-#include <geometry_msgs/TwistStamped.h>
 #include <rosflight_msgs/Barometer.h>
 #include <rosflight_msgs/Airspeed.h>
 #include <rosflight_msgs/Status.h>
 #include <math.h>
 #include <Eigen/Eigen>
-#include <numeric>
+
+#include <numeric> // for 18.04
 
 #define EARTH_RADIUS 6378145.0f
+#define FILTER_LENGTH 16
 
 namespace rosplane
 {
@@ -92,30 +95,45 @@ private:
   ros::NodeHandle nh_;
   ros::NodeHandle nh_private_;
   ros::Publisher vehicle_state_pub_;
-  ros::Subscriber gnss_fix_sub_;
-  ros::Subscriber gnss_vel_sub_; //used in conjunction with the gnss_fix_sub_
+  ros::Subscriber gps_sub_;
   ros::Subscriber imu_sub_;
   ros::Subscriber baro_sub_;
   ros::Subscriber airspeed_sub_;
   ros::Subscriber status_sub_;
+  ros::Subscriber inertial_sense_sub_;
 
+  void updateAltitudeAndAirspeed(const ros::TimerEvent &);
   void update(const ros::TimerEvent &);
-  void gnssFixCallback(const sensor_msgs::NavSatFix &msg);
-  void gnssVelCallback(const geometry_msgs::TwistStamped &msg);
+  // void gpsCallback(const rosflight_msgs::GPS &msg);
+  void gpsCallback(const inertial_sense::GPS &msg);
   void imuCallback(const sensor_msgs::Imu &msg);
   void baroAltCallback(const rosflight_msgs::Barometer &msg);
   void airspeedCallback(const rosflight_msgs::Airspeed &msg);
   void statusCallback(const rosflight_msgs::Status &msg);
+  void inertialSenseCallback(const nav_msgs::Odometry &msg_in);
+  void updateAirspeed(const ros::TimerEvent &);
+	void filterAirspeed(float &Va);
 
   double update_rate_;
   ros::Timer update_timer_;
-  std::string gnss_fix_topic_;
-  std::string gnss_vel_topic_;
+  std::string gps_topic_;
   std::string imu_topic_;
   std::string baro_topic_;
   std::string airspeed_topic_;
   std::string status_topic_;
 
+  float lpf_static_base_;
+  float lpf_diff_base_;
+  float Ve_base_;
+  float Vn_base_;
+  float alpha1_base_;
+  float Vahat_;
+  float hhat_;
+	float filter_taps_[FILTER_LENGTH] = {0.00970489747784546,	0.0145629416394102,	0.0282895307460455,	0.0485513149017384,
+		0.0718604720902472,	0.0941826681792385,	0.111642954778265,	0.121205220187210,	0.121205220187210,
+		0.111642954778265,	0.0941826681792385,	0.0718604720902472,	0.0485513149017384,	0.0282895307460455,
+		0.0145629416394102,	0.00970489747784546};
+	float Va_history_[FILTER_LENGTH];
   bool gps_new_;
   bool gps_init_;
   double init_lat_;       /**< Initial latitude in degrees */
@@ -129,6 +147,13 @@ private:
 
   struct params_s                 params_;
   struct input_s                  input_;
+
+
+  double height_offset_;
+  double calibrate_to_this_;
+  double calibration_sum_;
+  int avg_this_many_;
+  int counted_this_many_ ;
 };
 
 } //end namespace
